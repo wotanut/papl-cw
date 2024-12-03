0e91dfe9193bca4fa2d1d24b1d7cf9c0174cc23b
wotanut <sam@sambot.dev>
2024-12-03 13:59:57 +0000

    Add flight model

--- src/models/flight.py
+++ src/models/flight.py
@@ -3,3 +3,7 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 class Flight(SQLModel, table=True):
     id: int = Field(primary_key=True, unique=True)
     stage: str = Field(default="Departing") # TODO Limit to list of options
+    dep: str = Field() # TODO limit to 4 digits
+    dest: str = Field() # TODO limit to 4 digits
+    altn: str = Field()# TODO limit to 4 digits
+    ete: str = Field() # TODO force int in format xxxx
