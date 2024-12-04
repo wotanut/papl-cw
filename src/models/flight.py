2756f3e78297d30a130c9819a60bf5a021ec45cb
wotanut <sam@sambot.dev>
2024-12-04 12:14:26 +0000

    Change Flight ID type to string

--- src/models/flight.py
+++ src/models/flight.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class Flight(SQLModel, table=True):
-    id: int = Field(primary_key=True, unique=True)
+    id: str = Field(primary_key=True, unique=True) # ID can be a string as all callsigns will be unique
     stage: str = Field(default="Departing") # TODO Limit to list of options
     dep: str = Field() # TODO limit to 4 digits
     dest: str = Field() # TODO limit to 4 digits
