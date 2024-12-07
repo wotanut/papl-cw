4205e64632d3ccd4d0e13bf20d7640029e073f59
wotanut <sam@sambot.dev>
2024-12-07 21:56:06 +0000

    Add ADCReq field to Flight model for user ADC specification

--- src/models/flight.py
+++ src/models/flight.py
@@ -11,18 +11,19 @@ class Flight(SQLModel, table=True):
     stage: A value from FlightStage, default's to PreDep
     dep, dest, altn: A 4 digit ICAO code that is regex enforced. Ideally should be in uppercase anyway, but will return uppercase regardless of what you put in.
     ete: Enroute Time Expected, a 4 digit string that is formatted such as 0019 for each minute you are expected to fly.
+    ADCReq: Used to specify if the user should be asked for an ADC, defualt is calculated on the change to cruise and has a 50/50 chance of occuring. Specifying a value will override this.
 
     Examples
     --------
     > Flight(id="AAL1254", stage="Departing",dep="EGLL", dest="KJFK",altn="CYYZ",ete="0015")
 
-    > Flight(id="BAW15K", stage="Departing",dep="EGLL", dest="KJFK",altn="CYYZ",ete="0015")
+    > Flight(id="BAW15K", stage="Departing",dep="EGLL", dest="KJFK",altn="CYYZ",ete="0015",ADCReq=True)
 
-    > Flight(id="VIR27L", stage="Departing",dep="EGLL", dest="KJFK",altn="CYYZ",ete="0015")
+    > Flight(id="VIR27L", stage="Departing",dep="EGLL", dest="KJFK",altn="CYYZ",ete="0015",ADCReq=False)
 
 
     See More
-    ---
+    --------
     @flightStage
     """
     id: str = Field(primary_key=True, unique=True,max_length=7,min_length=7 ,description="Can be a string of 7 digits as all callsigns are unique") #NOTE - ID can be a string as all callsigns will be unique
@@ -31,7 +32,5 @@ class Flight(SQLModel, table=True):
     dest: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[A-Z]{4}$"))
     altn: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[A-Z]{4}$"))
     ete: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[0-9]{4}$"))
+    ADCReq: bool | None = Field(default=None, nullable=True) # ADC is a type of message
     #TODO: FK to Messages and FlightAware
-    #TODO: ADC REQ and ADC
-
-    
