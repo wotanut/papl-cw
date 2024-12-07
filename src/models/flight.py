f0574e642a2f5a57256b8ba99d03e883b82e92d4
wotanut <sam@sambot.dev>
2024-12-07 21:44:27 +0000

    Enhance Flight model with detailed field constraints and validation tests

--- src/models/flight.py
+++ src/models/flight.py
@@ -1,13 +1,27 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, Enum
-import enum
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, Enum, CheckConstraint
 from .flightStage import FlightStage
 
 class Flight(SQLModel, table=True):
-    id: str = Field(primary_key=True, unique=True) # ID can be a string as all callsigns will be unique
-    stage: FlightStage = Field(sa_column=Column(Enum(FlightStage)), default="PreDep") # TODO Limit to list of options
-    dep: str = Field() # TODO limit to 4 digits
-    dest: str = Field() # TODO limit to 4 digits
-    altn: str = Field()# TODO limit to 4 digits
-    ete: str = Field() # TODO force int in format xxxx
+    """
+    The Flight model. Some important information
+
+    Parameters
+    ---
+    ID: Must be exactly 7 characters with at least the first 3 being an ICAO code
+    stage: A value from FlightStage, default's to PreDep
+    
+
+    See More
+    ---
+
+    """
+    id: str = Field(primary_key=True, unique=True,max_length=7,min_length=7 ,description="Can be a string of 7 digits as all callsigns are unique") #NOTE - ID can be a string as all callsigns will be unique
+    stage: FlightStage = Field(sa_column=Column(Enum(FlightStage)), default="PreDep")
+    dep: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[A-Z]{4}$"))
+    dest: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[A-Z]{4}$"))
+    altn: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[A-Z]{4}$"))
+    ete: str = Field(max_length=4,min_length=4,nullable=False,sa_column_args=CheckConstraint(r"^[0-9]{4}$"))
     #TODO: FK to Messages and FlightAware
     #TODO: ADC REQ and ADC
+
+    
--- src/tests/test_flight.py
+++ src/tests/test_flight.py
@@ -63,3 +63,131 @@ def test_bad_stage():
             statement = select(Flight).where(Flight.id == flight["id"])
             results = session.exec(statement)
             assert results == None
+
+def test_no_dep():
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : "e",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015"
+            }# Invalid because no departure
+        with pytest.raises(Exception):
+            response = client.post(
+                '/flight/init', 
+                json=flight,
+            )
+            assert response.status_code == 400
+            
+            # Should'nt have been inserted
+
+            statement = select(Flight).where(Flight.id == flight["id"])
+            results = session.exec(statement)
+            assert results == None
+
+def test_malformatted_dep():
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : "e",
+                "dep" : "EGLLL",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015"
+            }# One extra L on departure
+        with pytest.raises(Exception):
+            response = client.post(
+                '/flight/init', 
+                json=flight,
+            )
+            assert response.status_code == 400
+            
+            # Should'nt have been inserted
+
+            statement = select(Flight).where(Flight.id == flight["id"])
+            results = session.exec(statement)
+            assert results == None
+
+def test_lowercase_dep():
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : FlightStage.PreDep.value,
+                "dep" : "egll",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015"
+            }# One extra L on departure
+        response = client.post(
+            '/flight/init', 
+            json=flight,
+        )
+        assert response.status_code == 200
+
+        statement = select(Flight).where(Flight.id == flight["id"])
+        results = session.exec(statement)
+        index = 0
+        for result in results:
+            assert result.id == flight["id"]
+            assert result.stage.value == flight["stage"]
+            assert result.dep == flight["dep"] #NOTE - Not testing for upper here because we're directly checking the DB
+            # If / when I make an endpoint for it I WILL just call that and that should return upper
+            assert result.dest == flight["dest"]
+            assert result.altn == flight["altn"]
+            assert result.ete == flight["ete"]
+            index += 1
+        assert index == 1
+        resetDB(session)
+
+
+def test_malformed_ete():
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : "e",
+                "dep" : "EGLLL",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015A"
+            }# One extra L on departure
+        with pytest.raises(Exception):
+            response = client.post(
+                '/flight/init', 
+                json=flight,
+            )
+            assert response.status_code == 400
+            
+            # Should'nt have been inserted
+
+            statement = select(Flight).where(Flight.id == flight["id"])
+            results = session.exec(statement)
+            assert results == None
+            
+def test_negative_ete():
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : "e",
+                "dep" : "EGLLL",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "-0115"
+            }# One extra L on departure
+        with pytest.raises(Exception):
+            response = client.post(
+                '/flight/init', 
+                json=flight,
+            )
+            assert response.status_code == 400
+            
+            # Should'nt have been inserted
+
+            statement = select(Flight).where(Flight.id == flight["id"])
+            results = session.exec(statement)
+            assert results == None
