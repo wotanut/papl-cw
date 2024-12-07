9b5681ac706689bbc0c11f342db3e20bd3a24d67
wotanut <sam@sambot.dev>
2024-12-07 15:14:05 +0000

    Add resetDB function to clear and reinitialize the database

--- src/db.py
+++ src/db.py
@@ -1,13 +1,12 @@
 import os
 
-from sqlmodel import create_engine, SQLModel, Session
-import models
-
+from sqlmodel import create_engine, SQLModel, Session, select
+from models.flight import Flight
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
-
+# WARNING: Remove before committing
 
 def init_db():
     SQLModel.metadata.create_all(engine)
@@ -16,3 +15,20 @@ def init_db():
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+def resetDB(session: Session):
+    """
+    Reset's the database to it's initial form
+
+    This is primarily a debug tool but could prove useful for an ADMIN ONLY API endpoint to allow admins to delete everything from the database and reinitialise it
+    """
+    with session:
+        flights = select(Flight)  # Selects all the flights
+        results = session.exec(flights)
+        for result in results:
+            session.delete(result)
+        session.commit()
+        session.close()
+
+    init_db()
