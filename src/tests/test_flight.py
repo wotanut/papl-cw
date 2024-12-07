d974b83f2a9ed081b21159ebe5da078e87272db8
wotanut <sam@sambot.dev>
2024-12-07 20:59:21 +0000

    move flight stage to its own folder for now (forgot to commit it as part of the other commit)

--- src/tests/test_flight.py
+++ src/tests/test_flight.py
@@ -1,43 +1,64 @@
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select
 
 from ..app import app
+from ..db import *
+from ..models.flight import FlightStage
 
 client = TestClient(app)
 
-def checkInserted(json, url:str = "/"):
-    response = client.get(url)
-    assert response.json() == json
-    assert response.status_code == 200
-
-# def test_table_exists():
-
-
 def test_good_flight_init():
-    flight ={
-            "id" : "BAW15K",
-            "stage" : "departing",
-            "dep" : "EGLL",
-            "dest" : "KJFK",
-            "altn" : "KBOS",
-            "ete" : "0015"
-        }
-    response = client.post(
-        '/flight/init', 
-        json=flight,
-    )
-    assert response.status_code == 200
-    checkInserted(flight,"/get/flight")
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : FlightStage.PreDep.value,
+                "dep" : "EGLL",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015"
+            }
+        response = client.post(
+            '/flight/init', 
+            json=flight,
+        )
+        assert response.status_code == 200
+        # Should've been inserted
+
+        statement = select(Flight).where(Flight.id == flight["id"])
+        results = session.exec(statement)
+        index = 0
+        for result in results:
+            assert result.id == flight["id"]
+            assert result.stage == flight["stage"]
+            assert result.dep == flight["dep"]
+            assert result.dest == flight["dest"]
+            assert result.altn == flight["altn"]
+            assert result.ete == flight["ete"]
+            index += 1
+        assert index == 1
+        resetDB(session)
+
+
 
 def test_bad_flight_init():
-    response = client.post(
-        '/flight/init', 
-        json={
-            "id" : "BAW15K",
-            "stage" : "e", 
-            "dep" : "EGLL",
-            "dest" : "KJFK",
-            "altn" : "KBOS",
-            "ete" : "0015"
-        },
-    ) # INVALID because stage can not be e
-    assert response.status_code == 200
+    with Session(engine) as session:
+        resetDB(session)
+        flight ={
+                "id" : "BAW15K",
+                "stage" : "e",
+                "dep" : "EGLL",
+                "dest" : "KJFK",
+                "altn" : "KBOS",
+                "ete" : "0015"
+            }# Invalid because stage can not be e
+        response = client.post(
+            '/flight/init', 
+            json=flight,
+        )
+        assert response.status_code == 200
+        # Should'nt have been inserted
+
+        statement = select(Flight).where(Flight.id == flight["id"])
+        results = session.exec(statement)
+        assert results == None
