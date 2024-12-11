25d82728ca17a5bc3eac48ab2959e0b7d719243f
wotanut <sam@sambot.dev>
2024-12-11 12:18:17 +0000

    Finally fixed the layout of defualt mcdu page

--- app/lib/mcduPage.dart
+++ app/lib/mcduPage.dart
@@ -15,29 +15,29 @@ class Mcdupage extends StatefulWidget {
 class _McdupageState extends State<Mcdupage> {
   @override
   Widget build(BuildContext context) {
+    // return Column(
+    //   children: [
+    //     for (int i = 0; i < widget.slkButtons.length; i++) widget.slkButtons[i],
+    //   ],
+    // );
     // NOTE - Can't use for each because need to jump up by two each time
     return Container(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          if (widget.slkButtons.length < 2)
+          for (int i = 0; i < widget.slkButtons.length; i += 2)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                widget.slkButtons[0],
+                widget.slkButtons[i],
+                widget.slkButtons[i + 1],
               ],
             )
-          else
-            for (int i = 0; i < widget.slkButtons.length; i += 2)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  widget.slkButtons[i],
-                  widget.slkButtons[i + 1],
-                ],
-              )
+            )
         ],
       ),
+      // TODO - Add Text Entry Box / Error Box here
+      // TODO - Add Text Entry Box / Error Box here
     );
   }
 }
