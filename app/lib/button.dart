b85afa6390d021bda36f4df2cd484d8cad453113
wotanut <sam@sambot.dev>
2024-12-11 12:23:09 +0000

    Add title parameter to mcduEntryBTN for customizable button text

--- app/lib/button.dart
+++ app/lib/button.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class mcduEntryBTN extends StatefulWidget {
   final int slk; // slk = Select Key
   final bool isRightSide;
+  final String title;
   // FIXME- Add link to page
 
-  const mcduEntryBTN({super.key, this.slk = 1, this.isRightSide = false});
+  const mcduEntryBTN(
+      {super.key, this.slk = 1, this.isRightSide = false, this.title = "Test"});
 
   @override
   State<mcduEntryBTN> createState() => _mcduEntryBTNState();
@@ -17,7 +19,7 @@ class _mcduEntryBTNState extends State<mcduEntryBTN> {
     return Container(
         decoration: BoxDecoration(border: Border.all(color: Colors.cyan)),
         child: TextButton(
-            child: const Text("test"),
+            child: Text(widget.title),
             onPressed: () {
               print("Clicked");
             }));
