ec55e5e41566561b9652bdf5fe5170306bf924d2
wotanut <sam@sambot.dev>
2024-12-17 16:14:58 +0000

    Added a few models

---
+++ src/models/message.py
@@ -0,0 +1,20 @@
+from typing import Literal, Union
+
+from sqlmodel import CheckConstraint, Column, Enum, Field, SQLModel
+
+
+class Message(SQLModel, table=True):
+    """
+    A message sent from a ground station to the aircraft
+
+
+    Parameters
+    ----------
+    :type: Either ATC or Company. Company would appear on the ECAM but can be sent by any ground staiton.
+    :sender: The station that sent it, for example LON_N_CTR, TELEX, BAWOPS, EZYOPS, etc..
+    :content: The content of the message, for example "ADC Required, send an ADC when convenient via ADV menu."
+    """
+    id: int = Field(primary_key=True, unique=True)
+    type: Literal["ATC", "Company"] = Field(nullable=False)
+    sender: str = Field(min_length=3,max_length=15) #NOTE - sender will appear as title for company msgs
+    content: str = Field(max_length=70)
