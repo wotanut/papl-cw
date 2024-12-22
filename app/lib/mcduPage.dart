793890e05c9ce89f7f2fce8d11e2b6202947ddd4
wotanut <sam@sambot.dev>
2024-12-22 23:30:54 +0000

    Basic layout of AOC flt init page

--- app/lib/mcduPage.dart
+++ app/lib/mcduPage.dart
@@ -16,25 +16,28 @@ class _McdupageState extends State<Mcdupage> {
   @override
   Widget build(BuildContext context) {
     // NOTE - Can't use for each because need to jump up by two each time
-    return Column(
-      children: [
-        if (widget.slkButtons.length < 2)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              widget.slkButtons[0],
-            ],
-          )
-        else
-          for (int i = 0; i < widget.slkButtons.length; i += 2)
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          if (widget.slkButtons.length < 2)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                widget.slkButtons[i],
-                widget.slkButtons[i + 1],
+                widget.slkButtons[0],
               ],
             )
-      ],
+          else
+            for (int i = 0; i < widget.slkButtons.length; i += 2)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  widget.slkButtons[i],
+                  widget.slkButtons[i + 1],
+                ],
+              )
+        ],
+      ),
     );
   }
 }
