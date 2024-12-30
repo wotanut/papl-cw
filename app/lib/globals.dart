6ed821bc2f640873eb154438477c1ce3f7b32f72
wotanut <sam@sambot.dev>
2024-12-30 19:15:33 +0000

    Add global variables

---
+++ app/lib/globals.dart
@@ -0,0 +1,9 @@
+library globals;
+// https://dart.dev/language/libraries
+// https://stackoverflow.com/questions/29182581/global-variables-in-dart
+// library essentially means that you don't have to use package: at the start of every import
+// Seemed like a smart way to think of the future
+
+String apiURL = "https://127.0.0.1"; // FIXME - Change to api.sambot.dev
+String appVersion =
+    "0.0.1-alpha"; //NOTE - Follows the Major Minor Path semantic versioning https://semver.org/
