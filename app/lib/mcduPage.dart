6f6c095decfe1937696d69fd52dfd9770548c1f2
wotanut <sam@sambot.dev>
2024-12-30 19:01:58 +0000

    fix error with mdcupage

--- app/lib/mcduPage.dart
+++ app/lib/mcduPage.dart
@@ -25,14 +25,22 @@ class _McdupageState extends State<Mcdupage> {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          for (int i = 0; i < widget.slkButtons.length; i += 2)
+          if (widget.slkButtons.length < 2)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                widget.slkButtons[i],
-                // widget.slkButtons[i + 1],
+                widget.slkButtons[0],
               ],
             )
+          else
+            for (int i = 0; i < widget.slkButtons.length; i += 2)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  widget.slkButtons[i],
+                  widget.slkButtons[i + 1],
+                ],
+              )
         ],
       ),
       // TODO - Add Text Entry Box / Error Box here
