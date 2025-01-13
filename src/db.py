d2b5fa55fa303a26e1304e3c00bc701b106cc802
Sam <sam@sambot.dev>
2025-01-13 15:47:39 +0000

    Code cleanup

--- src/db.py
+++ src/db.py
@@ -9,10 +9,12 @@ load_dotenv()
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
-# WARNING: Remove before committing
 
 
 def init_db():
+    """
+    Creates all the tables required for the database
+    """
     SQLModel.metadata.create_all(engine)
 
 
